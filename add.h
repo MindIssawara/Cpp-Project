e91d2d0b8af023463486e1ff127a90869b9de146
PAWEENNUTpuyfai <110964154+PAWEENNUTpuyfai@users.noreply.github.com>
2023-02-23 19:22:13 +0700

    add return nobody ส่งว่ามีหมากใน position ไหม

--- add.h
+++ add.h
@@ -10,8 +10,9 @@ using namespace std::this_thread;
 using namespace std::chrono;
 
 
-bool timeToRoll = 1,Finished=1;
+bool timeToRoll = 1, Finished = 1;
 int w = 50;
+int nobody = 0;
 int Round = 0;
 int red[4][2] = { {450,625},{550,625},{450,725},{550,725} };
 int green[4][2] = { {450,175},{550,175},{450,275},{550,275} };
@@ -37,110 +38,111 @@ void getstartM(int x, int y, int& mx, int& my) {
     }
 }
 
-void getstart(int xc, int yc ,int P) {
-	if (xc >= 450 && xc <= 500 && yc >= 175 && yc <= 225 && P==1 && !Finished){
-		getstartM(xc ,yc, green[0][0], green[0][1]);
-		timeToRoll = 1;
-		Finished = 1;
-	}
-    if (xc >= 550 && xc <= 600 && yc >= 175 && yc <= 225 && P==1 && !Finished){
-		getstartM(xc, yc, green[1][0], green[1][1]);
-		timeToRoll = 1;
-		Finished = 1;
-	}   
-    if (xc >= 450 && xc <= 500 && yc >= 275 && yc <= 325 && P==1 && !Finished){
-		getstartM(xc, yc, green[2][0], green[2][1]);
-		timeToRoll = 1;
-		Finished = 1;
-	}   
-    if (xc >= 550 && xc <= 600 && yc >= 275 && yc <= 325 && P==1 && !Finished){
-		getstartM(xc, yc, green[3][0], green[3][1]);
-		timeToRoll = 1;
-		Finished = 1;
-	}   
-    if (xc >= 900 && xc <= 950 && yc >= 175 && yc <= 225 && P==2 && !Finished){
-		getstartM(xc, yc, yellow[0][0], yellow[0][1]);
-		timeToRoll = 1;
-		Finished = 1;
-	}   
-    if (xc >= 1000 && xc <= 1050 && yc >= 175 && yc <= 225 && P==2 && !Finished){
-		getstartM(xc, yc, yellow[1][0], yellow[1][1]);
-		timeToRoll = 1;
-		Finished = 1;
-	} 
-    if (xc >= 900 && xc <= 950 && yc >= 275 && yc <= 325 && P==2 && !Finished){
-		getstartM(xc, yc, yellow[2][0], yellow[2][1]);
-		timeToRoll = 1;
-		Finished = 1;
-	}  
-    if (xc >= 1000 && xc <= 1050 && yc >= 275 && yc <= 325 && P==2 && !Finished) {
-		getstartM(xc, yc, yellow[3][0], yellow[3][1]);
-		timeToRoll = 1;
-		Finished = 1;
-	}
-    if (xc >= 900 && xc <= 950 && yc >= 625 && yc <= 675 && P==3 && !Finished){
-		getstartM(xc, yc, blue[0][0], blue[0][1]);
-		timeToRoll = 1;
-		Finished = 1;
-	}   
-    if (xc >= 1000 && xc <= 1050 && yc >= 625 && yc <= 675 && P==3 && !Finished){
-		getstartM(xc, yc, blue[1][0], blue[1][1]);
-		timeToRoll = 1;
-		Finished = 1;
-	} 
-    if (xc >= 900 && xc <= 950 && yc >= 725 && yc <= 775 && P==3 && !Finished){
-		getstartM(xc, yc, blue[2][0], blue[2][1]);
-		timeToRoll = 1;
-		Finished = 1;
-	}   
-    if (xc >= 1000 && xc <= 1050 && yc >= 725 && yc <= 775 && P==3 && !Finished){
-		getstartM(xc, yc, blue[3][0], blue[3][1]);
-		timeToRoll = 1;
-		Finished = 1;
-	}  
-    if (xc >= 450 && xc <= 500 && yc >= 625 && yc <= 675 && P==4 && !Finished){
-		getstartM(xc, yc, red[0][0], red[0][1]);
-		timeToRoll = 1;
-		Finished = 1;
-	}   
-    if (xc >= 550 && xc <= 600 && yc >= 625 && yc <= 675 && P==4 && !Finished){
-		getstartM(xc, yc, red[1][0], red[1][1]);
-		timeToRoll = 1;
-		Finished = 1;
-	}   
-    if (xc >= 450 && xc <= 500 && yc >= 725 && yc <= 775 && P==4 && !Finished){
-		getstartM(xc, yc, red[2][0], red[2][1]);
-		timeToRoll = 1;
-		Finished = 1;
-	}   
-    if (xc >= 550 && xc <= 600 && yc >= 725 && yc <= 775 && P==4 && !Finished){
-		getstartM(xc, yc, red[3][0], red[3][1]);
-		timeToRoll = 1;
-		Finished = 1;
-	}   
+void getstart(int xc, int yc, int P) {
+    if (xc >= 450 && xc <= 500 && yc >= 175 && yc <= 225 && P == 1 && !Finished) {
+        getstartM(xc, yc, green[0][0], green[0][1]);
+        timeToRoll = 1;
+        Finished = 1;
+    }
+    if (xc >= 550 && xc <= 600 && yc >= 175 && yc <= 225 && P == 1 && !Finished) {
+        getstartM(xc, yc, green[1][0], green[1][1]);
+        timeToRoll = 1;
+        Finished = 1;
+    }
+    if (xc >= 450 && xc <= 500 && yc >= 275 && yc <= 325 && P == 1 && !Finished) {
+        getstartM(xc, yc, green[2][0], green[2][1]);
+        timeToRoll = 1;
+        Finished = 1;
+    }
+    if (xc >= 550 && xc <= 600 && yc >= 275 && yc <= 325 && P == 1 && !Finished) {
+        getstartM(xc, yc, green[3][0], green[3][1]);
+        timeToRoll = 1;
+        Finished = 1;
+    }
+    if (xc >= 900 && xc <= 950 && yc >= 175 && yc <= 225 && P == 2 && !Finished) {
+        getstartM(xc, yc, yellow[0][0], yellow[0][1]);
+        timeToRoll = 1;
+        Finished = 1;
+    }
+    if (xc >= 1000 && xc <= 1050 && yc >= 175 && yc <= 225 && P == 2 && !Finished) {
+        getstartM(xc, yc, yellow[1][0], yellow[1][1]);
+        timeToRoll = 1;
+        Finished = 1;
+    }
+    if (xc >= 900 && xc <= 950 && yc >= 275 && yc <= 325 && P == 2 && !Finished) {
+        getstartM(xc, yc, yellow[2][0], yellow[2][1]);
+        timeToRoll = 1;
+        Finished = 1;
+    }
+    if (xc >= 1000 && xc <= 1050 && yc >= 275 && yc <= 325 && P == 2 && !Finished) {
+        getstartM(xc, yc, yellow[3][0], yellow[3][1]);
+        timeToRoll = 1;
+        Finished = 1;
+    }
+    if (xc >= 900 && xc <= 950 && yc >= 625 && yc <= 675 && P == 3 && !Finished) {
+        getstartM(xc, yc, blue[0][0], blue[0][1]);
+        timeToRoll = 1;
+        Finished = 1;
+    }
+    if (xc >= 1000 && xc <= 1050 && yc >= 625 && yc <= 675 && P == 3 && !Finished) {
+        getstartM(xc, yc, blue[1][0], blue[1][1]);
+        timeToRoll = 1;
+        Finished = 1;
+    }
+    if (xc >= 900 && xc <= 950 && yc >= 725 && yc <= 775 && P == 3 && !Finished) {
+        getstartM(xc, yc, blue[2][0], blue[2][1]);
+        timeToRoll = 1;
+        Finished = 1;
+    }
+    if (xc >= 1000 && xc <= 1050 && yc >= 725 && yc <= 775 && P == 3 && !Finished) {
+        getstartM(xc, yc, blue[3][0], blue[3][1]);
+        timeToRoll = 1;
+        Finished = 1;
+    }
+    if (xc >= 450 && xc <= 500 && yc >= 625 && yc <= 675 && P == 4 && !Finished) {
+        getstartM(xc, yc, red[0][0], red[0][1]);
+        timeToRoll = 1;
+        Finished = 1;
+    }
+    if (xc >= 550 && xc <= 600 && yc >= 625 && yc <= 675 && P == 4 && !Finished) {
+        getstartM(xc, yc, red[1][0], red[1][1]);
+        timeToRoll = 1;
+        Finished = 1;
+    }
+    if (xc >= 450 && xc <= 500 && yc >= 725 && yc <= 775 && P == 4 && !Finished) {
+        getstartM(xc, yc, red[2][0], red[2][1]);
+        timeToRoll = 1;
+        Finished = 1;
+    }
+    if (xc >= 550 && xc <= 600 && yc >= 725 && yc <= 775 && P == 4 && !Finished) {
+        getstartM(xc, yc, red[3][0], red[3][1]);
+        timeToRoll = 1;
+        Finished = 1;
+    }
 }
 
-int *serchx(int x, int y) {
-	for (int i = 0; i < 4; i++) {
-		if (red[i][0] == x && red[i][1] == y) return &red[i][0];
-		if (blue[i][0] == x && blue[i][1] == y) return &blue[i][0];
-		if (yellow[i][0] == x && yellow[i][1] == y) return &yellow[i][0];
-		if (green[i][0] == x && green[i][1] == y) return &green[i][0];
-	}
+int* serchx(int x, int y) {
+    for (int i = 0; i < 4; i++) {
+        if (red[i][0] == x && red[i][1] == y) return &red[i][0];
+        if (blue[i][0] == x && blue[i][1] == y) return &blue[i][0];
+        if (yellow[i][0] == x && yellow[i][1] == y) return &yellow[i][0];
+        if (green[i][0] == x && green[i][1] == y) return &green[i][0];
+    }return &nobody;
 }
 int* serchy(int x, int y) {
-	for (int i = 0; i < 4; i++) {
-		if (red[i][0] == x && red[i][1] == y) return &red[i][1];
-		if (blue[i][0] == x && blue[i][1] == y) return &blue[i][1];
-		if (yellow[i][0] == x && yellow[i][1] == y) return &yellow[i][1];
-		if (green[i][0] == x && green[i][1] == y) return &green[i][1];
-	}
+    for (int i = 0; i < 4; i++) {
+        if (red[i][0] == x && red[i][1] == y) return &red[i][1];
+        if (blue[i][0] == x && blue[i][1] == y) return &blue[i][1];
+        if (yellow[i][0] == x && yellow[i][1] == y) return &yellow[i][1];
+        if (green[i][0] == x && green[i][1] == y) return &green[i][1];
+    }return &nobody;
+    
 }
 
 int move_red(int& x, int& y, int d) {
     for (int i = 0; i < d; i++) {
         if (x == 675 && y >= 600 && y <= 800) y -= w;
-       	else if (x == 675 && y == 550) {
+        else if (x == 675 && y == 550) {
             x = 625;
             y = 500;
         }
