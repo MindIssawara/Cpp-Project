dcf77437a45be5da39081c7b948e66fff805d521
penpizza <110964198+penpizza@users.noreply.github.com>
2023-03-10 12:57:53 +0700

    แก้บัคสีแดงและเหลือง

--- add.h
+++ add.h
@@ -306,7 +306,14 @@ int move_red(int& x, int& y, int d) {
         else if (x == 775 && y >= 550 && y <= 750) y += w;
         else if (y == 800 && x == 775) x -= w;
         else if (y == 800 && x == 725) y -= w;
-        else if (x == 725 && y >= 500 && y <= 750)  y -= w;
+        else if (x == 725 && y >= 500 && y <= 750)  {
+            y -= w;
+        if (i== 5) {
+            Finished = 1;
+            timeToRoll = 1;
+            return 1;
+        }
+                                                   
     }
     Finished = 1;
     timeToRoll = 1;
@@ -355,7 +362,11 @@ int move_yellow(int& x, int& y, int d) {
         else if (x == 675 && y >= 150 && y <= 350) y -= w;
         else if (y == 100 && x == 675) x += w;
         else if (y == 100 && x == 725) y += w;
-        else if (x == 725 && y >= 150 && y <= 400) y += w;
+        else if (x == 725 && y >= 150 && y <= 400) {y += w
+           if (i== 5) {
+            Finished = 1;
+            timeToRoll = 1;
+            return 1;  }
     }
     Finished = 1;
     timeToRoll = 1;
